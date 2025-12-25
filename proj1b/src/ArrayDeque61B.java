4c1d790731c546f75b749144946500b691c213ac
Tom David <sl20113321@163.com>
2025-12-25 16:43:31 +0800

    完成了proj1b中，ArrayDeque61B的接口实现、实例属性、构造器、addFirst、addLast、get方法的实现

---
+++ proj1b/src/ArrayDeque61B.java
@@ -0,0 +1,69 @@
+import java.util.List;
+import static java.lang.Math.floorMod;
+
+public class ArrayDeque61B<T> implements Deque61B<T> {
+
+    private int size;
+    private T[] items;
+    private int nextFirst;
+    private int nextLast;
+
+    public ArrayDeque61B(){
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+        items = (T[]) new Object[8];
+    }
+
+    @Override
+    public void addFirst(T x) {
+        items[nextFirst] = x;
+        nextFirst = floorMod(nextFirst - 1, items.length);
+        size += 1;
+    }
+
+    @Override
+    public void addLast(T x) {
+        items[nextLast] = x;
+        nextLast = floorMod(nextLast + 1, items.length);
+        size += 1;
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        if (index >= size || index < 0){
+            return null;
+        }
+        return items[floorMod(index + nextFirst + 1, items.length)];
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
