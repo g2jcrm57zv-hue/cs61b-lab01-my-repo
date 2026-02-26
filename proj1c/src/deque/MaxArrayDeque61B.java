eadb37b0c9e47d1eeae3638faf73db6c8036d0c9
Tom David <sl20113321@163.com>
2026-02-26 16:05:37 +0800

    完成proj1c的MaxArrayDeque61B的max方法与带Comparator的构造器方法,以及max方法的测试文件

---
+++ proj1c/src/deque/MaxArrayDeque61B.java
@@ -0,0 +1,35 @@
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque61B<T> extends ArrayDeque61B<T> {
+
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque61B(Comparator<T> c){
+        super();
+        comparator = c;
+    }
+
+    public T max() {
+        return max(this.comparator);
+    }
+
+    public T max(Comparator<T> c) {
+        // 1.deque无元素时,直接返回null.
+        if (this.isEmpty()) {return null;}
+
+        // 2.若有元素,经过比较,返回最大值
+        T max = this.get(0);
+        for (T d : this){
+            if (c.compare(max, d) < 0){
+                max = d;
+            }
+        }
+
+        return max;
+    }
+
+
+
+}
--- proj1c/tests/MaxArrayDeque61BTest.java
+++ proj1c/tests/MaxArrayDeque61BTest.java
@@ -1,10 +1,12 @@
-import org.junit.jupiter.api.*;
+import deque.Deque61B;
+import org.junit.Test;
 
 import java.util.Comparator;
 import deque.MaxArrayDeque61B;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth.assertWithMessage;
+import static org.junit.Assert.assertEquals;
 
 public class MaxArrayDeque61BTest {
     private static class StringLengthComparator implements Comparator<String> {
@@ -20,5 +22,21 @@ public class MaxArrayDeque61BTest {
         mad.addFirst("2");
         mad.addFirst("fury road");
         assertThat(mad.max()).isEqualTo("fury road");
+    }
+
+    @Test
+    public void testMax() {
+        Comparator<Integer> c = Comparator.naturalOrder();
+
+        MaxArrayDeque61B<Integer> maxArray = new MaxArrayDeque61B<>(c);
+
+        maxArray.addFirst(10);
+        maxArray.addLast(2);
+        maxArray.addLast(4);
+        maxArray.addLast(87);
+        maxArray.addLast(13);
+        maxArray.addLast(32);
+
+        assertEquals((Integer) 87, maxArray.max());
     }
 }
